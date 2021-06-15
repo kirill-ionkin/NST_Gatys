6b511d4d6af2b81053aed08558edfa9223523056
Kirill Ionkin <kirill-ionkin@mail.ru>
2021-06-15 16:36:02 +0300

    Create and edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# NST_Gatys
+# A Neural Algorithm of Artistic Style
+
+- [https://arxiv.org/abs/1508.06576](https://arxiv.org/abs/1508.06576)
+- [Breaking Down Leon Gatys’ Neural Style Transfer in PyTorch](https://towardsdatascience.com/breaking-down-leon-gatys-neural-style-transfer-in-pytorch-faf9f0eb79db)
+
+
+## All results you can find here:
+ 
+- [Основной рабочий jupyter notebook - NST_Gatys_working_notebook.ipynb](./NST_Gatys_working_notebook.ipynb)
+- [Готовый к использованию jupyter notebook - Colab_working_notebook.ipynb](./Colab_working_notebook.ipynb)
+
+
+## Results
+
+#### First experiments with style image(2000 steps optimizing input image)
+For the content image, I chose my own photo. It's me at the climbing wall in Moscow, Limestone.
+
+Content Image
+
+![](./example/content.jpg)
+
+Style Image
+
+![](./example/style.jpg)
+
+###### Results(input image - content image).
+
+Style weight = 1e4, 1e5, 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style/collage_style.png)
+
+###### Results(input image - Gaussian noise). 
+
+Style weight = 1e4, 1e5, 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style/collage_noise.png)
+
+
+#### Other styles(1000 steps optimizing input image)
+
+##### Style image №1
+
+![](./example/style_1.jpg)
+
+###### Results(input image - content image).
+
+Style weight = 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style_1/collage_style_1.png)
+
+
+##### Style image №2
+
+![](./example/style_2.jpg)
+
+###### Results(input image - content image).
+
+Style weight = 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style_2/collage_style_2.png)
+
+
+##### Style image №3
+
+![](./example/style_3_sliced.jpg)
+
+###### Results(input image - content image).
+
+Style weight = 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style_3/collage_style_3.png)
+
+
+##### Style image №4
+
+![](./example/style_4_sliced.jpg)
+
+###### Results(input image - content image).
+
+Style weight = 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style_4/collage_style_4.png)
+
+
+##### Style image №5
+
+![](./example/style_5_sliced.jpg)
+
+###### Results(input image - content image).
+
+Style weight = 1e6, 1e7.
+Content weight(constant) = 1.
+![](./results/style_5/collage_style_5.png)
--- create_train_model.py
+++ create_train_model.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 
@@ -10,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 from layers import Normalization, ContentLoss, StyleLoss
-from utils import imshow, unloader
+from utils import imshow
 
 
 content_layers_default = ["conv4_2"]
@@ -58,96 +57,103 @@ vgg_19_layers_names = {
     33: "relu5_3",
     34: "conv5_4",
     35: "relu5_4",
-    36: "pool5"
+    36: "pool5",
 }
 
 
-def create_nst_model(pretrained_vgg19_model,
-                     content_img, 
-                     style_img,
-                     normalization_mean,
-                     normalization_std,
-                     pooling_mode = "avg",
-                     content_layers=content_layers_default,
-                     style_layers=style_layers_default,
-                     device=device
-                    ):
-    
+def create_nst_model(
+    pretrained_vgg19_model,
+    content_img,
+    style_img,
+    normalization_mean,
+    normalization_std,
+    pooling_mode="avg",
+    content_layers=content_layers_default,
+    style_layers=style_layers_default,
+    device=device,
+):
+
     normalization = Normalization(normalization_mean, normalization_std).to(device)
-    
+
     nst_model = nn.Sequential(normalization)
     content_loss_layers = []
     style_loss_layers = []
-    
+
     c = 1
     s = 1
     for i, layer_i in enumerate(pretrained_vgg19_model.features.children()):
         name_layer_i = vgg_19_layers_names[i]
-        
+
         if (name_layer_i.startswith("pool")) and (pooling_mode == "avg"):
-            layer = nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=False)
-        
+            layer_i = nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=False)
+
         if name_layer_i.startswith("relu"):
             layer_i = nn.ReLU(inplace=False)
-            
+
         nst_model.add_module(name_layer_i, layer_i)
-                
+
         if name_layer_i in content_layers:
             content_target = nst_model(content_img)
             content_loss_layer = ContentLoss(content_target)
             nst_model.add_module("content_loss_layer_{}".format(c), content_loss_layer)
             c += 1
-            
+
             content_loss_layers.append(content_loss_layer)
-            
+
         if name_layer_i in style_layers:
             style_target = nst_model(style_img)
             style_loss_layer = StyleLoss(style_target)
             nst_model.add_module("style_loss_layer_{}".format(s), style_loss_layer)
             s += 1
-            
+
             style_loss_layers.append(style_loss_layer)
-        
-        if (len(style_loss_layers) == len(style_layers)) and (len(content_loss_layers) == len(content_layers)):
+
+        if (len(style_loss_layers) == len(style_layers)) and (
+            len(content_loss_layers) == len(content_layers)
+        ):
             break
-    
+
     for content_loss_layer in content_loss_layers:
         content_loss_layer.mode = "loss"
-    
+
     for style_loss_layer in style_loss_layers:
         style_loss_layer.mode = "loss"
-    
+
     # to save video-memory
     torch.cuda.empty_cache()
-    
+
     return nst_model, content_loss_layers, style_loss_layers
 
 
 def get_input_optimizer(input_img):
-        optimizer = optim.LBFGS([input_img.requires_grad_()]) 
-        return optimizer
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
 
 
-def run_style_transfer(pretrained_vgg19_model,
-                       content_img,
-                       style_img,
-                       input_img=None,
-                       save_folder=None,
-                       normalization_mean=vgg19_normalization_mean,
-                       normalization_std=vgg19_normalization_std,
-                       content_layers=content_layers_default,
-                       style_layers=style_layers_default,
-                       num_steps=300,
-                       style_weight=1e5,
-                       content_weight=1,
-                       return_history_loss=False):
-    
+def run_style_transfer(
+    pretrained_vgg19_model,
+    content_img,
+    style_img,
+    input_img=None,
+    save_folder=None,
+    normalization_mean=vgg19_normalization_mean,
+    normalization_std=vgg19_normalization_std,
+    content_layers=content_layers_default,
+    style_layers=style_layers_default,
+    num_steps=300,
+    style_weight=1e5,
+    content_weight=1,
+    return_history_loss=False,
+):
+
     if input_img is None:
-        input_img = torch.randn(size=content_img.size(), requires_grad=True, device=device)
-    
+        input_img = torch.randn(
+            size=content_img.size(), requires_grad=True, device=device
+        )
+
     start = time.time()
-    print('Building the style transfer model..')
-    
+    print("Building the style transfer model..")
+
     nst_model, content_loss_layers, style_loss_layers = create_nst_model(
         pretrained_vgg19_model=pretrained_vgg19_model,
         content_img=content_img,
@@ -155,15 +161,15 @@ def run_style_transfer(pretrained_vgg19_model,
         normalization_mean=normalization_mean,
         normalization_std=normalization_std,
         content_layers=content_layers,
-        style_layers=style_layers
+        style_layers=style_layers,
     )
-    
+
     for p in nst_model.parameters():
         p.requires_grad = False
-    
+
     optimizer = get_input_optimizer(input_img)
-    
-    print('Optimizing..')
+
+    print("Optimizing..")
     run = [0]
     while run[0] <= num_steps:
 
@@ -190,29 +196,35 @@ def run_style_transfer(pretrained_vgg19_model,
 
             run[0] += 1
             if run[0] % 50 == 0:
-                #print("run {}:".format(run))
-                #print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                # print("run {}:".format(run))
+                # print('Style Loss : {:4f} Content Loss: {:4f}'.format(
                 #    all_style_loss, all_content_loss))
-                #print()
+                # print()
                 string_to_print = "run {} steps \n".format(run[0])
                 string_to_print += "Style Loss: {:.2f} Content Loss: {:.2f} \n".format(
-                        all_style_loss.item(), all_content_loss.item())
+                    all_style_loss.item(), all_content_loss.item()
+                )
                 string_to_print += "Spend time: {:.1f}".format(time.time() - start)
-                
+
                 plt.figure()
                 if save_folder is None:
                     imshow(input_img, title=string_to_print)
                 else:
-                    imshow(input_img, title=string_to_print,
-                           save_folder=save_folder,
-                           num_step=run[0],
-                           style_weight=style_weight,
-                           content_weight=content_weight)
-                    
+                    imshow(
+                        input_img,
+                        title=string_to_print,
+                        save_folder=save_folder,
+                        num_step=run[0],
+                        style_weight=style_weight,
+                        content_weight=content_weight,
+                    )
+
                 if return_history_loss:
                     global history_loss
-                    history_loss.append((all_content_loss.item(), all_style_loss.item()))
-                    
+                    history_loss.append(
+                        (all_content_loss.item(), all_style_loss.item())
+                    )
+
             return all_style_loss + all_content_loss
 
         optimizer.step(closure)
